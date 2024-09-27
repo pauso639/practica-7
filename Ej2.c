fccaba711f2def4ffc5aa096a90954707e314085
pauso639 <sofiapaulinalopezgonzalez@gmail.com>
2024-09-27 14:15:12 -0600

    Update Ej2.c

--- Ej2.c
+++ Ej2.c
@@ -1,44 +1,40 @@
 #include <stdio.h>
-int main()
+int main ()
 {
   int uno, dos, tres;
-  printf ("Ingrese 3 números separados por espacios:\n");
+  printf ("ingrese 3 numeros numeros separados por espacios:\n");
   scanf ("%d %d %d", &uno, &dos, &tres);
   if (uno > dos)
   {
-  if (dos > tres)
-  {
-  printf("%d es mayor a %d que es mayor a %d\n", uno, dos, tres);
+    if (dos > tres)
+    {
+      printf("%d es mayor a %d que es mayor a %d\n", uno, dos, tres);
+    }
+    else
+    {
+      if (uno > tres)
+      {
+        printf("%d es mayor a %d que es mayor a %d\n", tres, uno, dos);
+      }
+    }
   }
   else
   {
-  if (uno > tres)
-  {
-  printf("%d es mayor a %d que es mayor a %d\n", uno, tres, dos);
+    if (dos > tres)
+    {
+      if (tres > uno)
+      {
+        printf("%d es mayor a %d que es mayor a %d\n", dos, tres, uno);
+      }
+      else
+      {
+        printf("%d es mayor a %d que es mayor a %d\n", dos, uno, tres);
+      }
+    }
+    else
+    {
+      printf("%d es mayor a %d que es mayor a %d\n", tres, dos, uno);
+    }
   }
-  else
-  {
-  printf("%d es mayor a %d que es mayor a %d\n", tres, uno, dos);
-  }
-  }
-  }
-  else
-  {
-  if (dos > tres)
-  {
-if (tres > uno)
-  {
-  printf("%d es mayor a %d que es mayor a %d\n", dos, tres, uno);
-  }
-  else
-  {
-  printf("%d es mayor a %d que es mayor a %d\n", dos, uno, tres);
-  }
-  }
-  else
-  {
-  printf("%d es mayor a %d que es mayor a %d\n", tres, dos, uno);
-  }
-  }
-  return 0;  
+  return 0;
 }
